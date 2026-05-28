0c9713274ec2d7d11efdf514585bd14c17d49c24
RomaruDaze <romarudazee99@gmail.com>
2026-05-28 14:25:50 +0900

    Removing app.css imports from app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import "./App.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
